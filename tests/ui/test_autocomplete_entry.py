2b4c74c0b737afb392502a3102c884022da836b3
pathway27 <pathway27@gmail.com>
2015-08-10 23:36:46 +1000

    Added skeleton for testing ui; made module level init files; added pycache to gitignore

--- tests/ui/test_autocomplete_entry.py
+++ tests/ui/test_autocomplete_entry.py
@@ -1,7 +1,16 @@
+from revolver.ui.autocomplete_entry import AutoCompleteEntry
 import unittest
 
-class AutoCompleteEntryFunctionTest(unittest.TestCase):
-    pass
+class AutoCompleteEntryTest(unittest.TestCase):
+
+    def create(self, **kwargs):
+        return AutoCompleteEntry(self.root, **kwargs)
+
+    def test_blah(self):
+        self.assertEqual('foo'.upper(), 'FOO')
 
 class AutoCompleteEntryPerformanceTest(unittest.TestCase):
     pass
+
+if __name__ == '__main__':
+    unittest.main()
