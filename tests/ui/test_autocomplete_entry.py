e5b561f85edbab0a292e091223c8c3443b7c500d
pathway27 <pathway27@gmail.com>
2015-08-11 23:41:34 +1000

    Added more test cases

--- tests/ui/test_autocomplete_entry.py
+++ tests/ui/test_autocomplete_entry.py
@@ -2,12 +2,34 @@ from revolver.ui.autocomplete_entry import AutoCompleteEntry
 import unittest
 
 class AutoCompleteEntryTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
 
     def create(self, **kwargs):
         return AutoCompleteEntry(self.root, **kwargs)
 
-    def test_blah(self):
+    def test_basic_autocomplete_entry(self):
         self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_auto_complete_list_initalized(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_custom_matches_function_initalized(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_correct_autocomplete_results(self):
+        pass
 
 class AutoCompleteEntryPerformanceTest(unittest.TestCase):
     pass
