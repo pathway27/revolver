f5aed079fb4fcd6735ba55e4f71253a19011e8c3
pathway27 <pathway27@gmail.com>
2015-08-20 22:51:20 +1000

    test: coverage complete of autocomplete entry

--- tests/ui/test_autocomplete_entry.py
+++ tests/ui/test_autocomplete_entry.py
@@ -1,7 +1,7 @@
 from tkinter import Tk, StringVar
 from revolver.ui.autocomplete_entry import AutoCompleteEntry
 import unittest
-import types
+from types import MethodType, FunctionType
 
 class AutoCompleteEntryTest(unittest.TestCase):
     @classmethod
@@ -17,16 +17,19 @@ class AutoCompleteEntryTest(unittest.TestCase):
 
         tk_root = Tk()
         sv = StringVar()
+        sv.set("hello")
 
         def matches(fieldValue, acListEntry):
             pattern = re.compile(re.escape(fieldValue) + '.*')
             return re.match(pattern, acListEntry)
 
-        self.entry = AutoCompleteEntry(list_of_names, tk_root,
-                                       matches_function=matches)
-        self.entry.grid(row=0, column=0)
+        self.entry_default_args = AutoCompleteEntry(list_of_names, tk_root)
 
-        # tk_root.mainloop()
+
+        self.entry_custom_args = AutoCompleteEntry(list_of_names, tk_root,
+                                                   list_box_length=12,
+                                                   matches_function=matches,
+                                                   textvariable=sv)
 
     def tearDown(self):
         pass
@@ -34,18 +37,37 @@ class AutoCompleteEntryTest(unittest.TestCase):
     def create(self, **kwargs):
         return AutoCompleteEntry(self.root, **kwargs)
 
-    def test_basic_autocomplete_entry(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
     def test_auto_complete_list_initalized(self):
-        self.assertEqual(self.entry.autocomplete_list,
+        self.assertEqual(self.entry_default_args.autocomplete_list,
+                         [str(x) for x in range(60000)])
+        self.assertEqual(self.entry_custom_args.autocomplete_list,
                          [str(x) for x in range(60000)])
 
-    def test_custom_matches_function_initalized(self):
-        self.assertIsInstance(self.entry.matches_function, types.FunctionType)
+    def test_default_list_box_length_initalized(self):
+        self.assertEqual(self.entry_default_args.list_box_length, 8)
 
-    def test_correct_autocomplete_results(self):
-        pass
+    def test_default_matches_function_initalized(self):
+        self.assertIsInstance(self.entry_default_args.matches_function,
+                              MethodType)
+
+    def test_default_textvariable_initalized(self):
+        self.assertEqual(self.entry_default_args.textvariable.get(), '')
+
+    def test_default_matches_function_results(self):
+        self.entry_default_args.default_match('hi', 'hi')
+
+    def test_custom_list_box_length_initalized(self):
+        self.assertEqual(self.entry_custom_args.list_box_length, 12)
+
+    def test_custom_matches_function_initalized(self):
+        self.assertIsInstance(self.entry_custom_args.matches_function,
+                              FunctionType)
+
+    def test_custom_textvariable_initalized(self):
+        self.assertEqual(self.entry_custom_args.textvariable.get(), 'hello')
+
+    def test_basic_autocomplete_entry(self):
+        self.assertEqual('foo'.upper(), 'FOO')
 
 class AutoCompleteEntryPerformanceTest(unittest.TestCase):
     pass
